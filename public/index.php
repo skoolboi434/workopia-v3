235f10f88ad19d3c7bd903be548ba12ea185276f
Travis Davis <skoolboi434@gmail.com>
2024-01-18 12:00:13 -0500

    Added missing files

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+require '../helpers.php';
+require basePath('Database.php');
+require basePath('Router.php');
+
+// Instantiating the router
+$router = new Router();
+
+// Get routes
+$routes = require basePath('routes.php');
+
+// Get current URI and HTTP Method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Route the request
+$router->route($method, $uri);
