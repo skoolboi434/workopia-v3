96406f2e8cc541e90507308ef646c6fdeaa05c1b
Travis Davis <skoolboi434@gmail.com>
2024-01-18 11:54:27 -0500

    Initial Commit

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+  'host' => 'localhost',
+  'port' => 3306,
+  'dbname' => 'workopia-v2',
+  'username' => 'root',
+  'password' => 'password'
+];
---
+++ helpers.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Get the base path
+ * 
+ * @param string $path
+ * @return string
+ */
+
+function basePath($path = '')
+{
+  return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name, $data = [])
+{
+  $viewPath = basePath("views/{$name}.view.php");
+
+  if (file_exists($viewPath)) {
+    // Get data into view
+    extract($data);
+    require $viewPath;
+  } else {
+    echo "View '{$name} not found'";
+  }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+  $partialPath = basePath("views/partials/{$name}.php");
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    echo "Partial '{$name} not found'";
+  }
+}
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
+
+/**
+ * Format salary
+ * 
+ * @param string $salary
+ * 
+ * @return string Formatted Salary
+ */
+
+function formatSalary($salary)
+{
+  return '$' . number_format(floatval($salary));
+}
