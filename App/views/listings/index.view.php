607bd873d7f2cd3b10efa88160b861ea9fbb0d4d
Travis Davis <skoolboi434@gmail.com>
2024-01-20 17:25:44 -0500

    Search functionality added

--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -7,7 +7,13 @@
 <section>
   <div class="container mx-auto p-4 mt-4">
     <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">
-      All Jobs</div>
+      <?php if (isset($keywords)): ?>
+      Search Results for:
+      <?= htmlspecialchars($keywords) ?>
+      <?php else: ?>
+      All Jobs
+      <?php endif; ?>
+    </div>
     <?= loadPartial('message'); ?>
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
